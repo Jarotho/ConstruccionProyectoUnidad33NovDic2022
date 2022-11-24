ade28c7c41a378d44869239b0b9e22ddfacf922d
Jaroth Moo <89890819+Jarotho@users.noreply.github.com>
2022-11-24 13:35:08 -0600

    Add files via upload

--- ProyectoU3/src/App.java
+++ ProyectoU3/src/App.java
@@ -1,5 +1,27 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import GUI.Controlador.*;
+import GUI.Vista.Vista;
+import Json.*;
+
+/**
+ *
+ * @author jonat
+ */
+public class App{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        lecturaJson v = new lecturaJson("Empleados.json");
+        convertidorEmpleado converter = new convertidorEmpleado();
+        converter.convertJSONtoArray(v.getArrayJSONobject());
+
+       Vista VistaPrincipal = new Vista();
+        Controlador control = new Controlador(VistaPrincipal, converter.getarrayEmpleado());
+
+        VistaPrincipal.setVisible(true);
+
+
     }
+    
 }
